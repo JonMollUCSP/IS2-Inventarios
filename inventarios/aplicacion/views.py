2d60447ba555cf6e597a9863002d15e7b2f6d094
JonMoll <jonathan.mollapaza@ucsp.edu.pe>
2017-04-21 19:32:06 -0500

    Anadiendo formulario para anadir proveedores

--- inventarios/aplicacion/views.py
+++ inventarios/aplicacion/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 
-from .forms import IniciarSesionForm
-from .models import Usuario
+from .forms import *
+from .models import *
 
 def IniciarSesionView(request):
 	formulario = IniciarSesionForm(request.POST or None)
@@ -25,3 +25,22 @@ def IniciarSesionView(request):
 
 def InicioView(request):
 	return render(request, "inicio.html", {})
+
+def RegistrarProveedorView(request):
+	formulario = RegistrarProveedorForm(request.POST or None)
+	contexto = { "formulario" : formulario }
+
+	if formulario.is_valid():
+		print(formulario.cleaned_data)
+
+		datos_formulario = formulario.cleaned_data
+		nombre_obtenido = datos_formulario.get("nombre_form")
+		telefono_obtenido = datos_formulario.get("telefono_form")
+		direccion_obtenida = datos_formulario.get("direccion_form")
+		email_obtenido = datos_formulario.get("email_form")
+
+		objeto_proveedor = Proveedor.objects.create(nombre = nombre_obtenido, telefono = telefono_obtenido, direccion = direccion_obtenida, email = email_obtenido)
+
+		return HttpResponseRedirect(reverse('inicio'))
+		
+	return render(request, "registrar_proveedor.html", contexto)
