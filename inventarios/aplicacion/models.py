1dd0a1736f077c353e8f9a48850cb35286560f89
ltch23 <luistong23@gmai.com>
2017-05-05 09:35:40 -0500

    models para  proveedor, producto y proveedorproducto

--- inventarios/aplicacion/models.py
+++ inventarios/aplicacion/models.py
@@ -16,27 +16,27 @@ class Proveedor(models.Model):
 	direccion = models.TextField()
 	email = models.EmailField()
 
-	"""def __str__(self):
-		return self.id
-"""
+	def __str__(self):
+		return self.nombre
+
 class Producto(models.Model):
 	id = models.AutoField(primary_key= True)
 	nombre = models.CharField(max_length = 20)
 	tipo = models.CharField(max_length = 20)
 	valor = models.IntegerField()
 	
-	"""def __str__(self):
-		return self.id
-		"""
+	def __str__(self):
+		return self.nombre
+
 class ProveedorProducto(models.Model):
 	id = models.AutoField(primary_key=True)
 	proveedor = models.ForeignKey(Proveedor)
 	producto = models.ForeignKey(Producto)
 	fecha_tiempo = models.DateTimeField(auto_now_add=True)
 
-	"""def __str__(self):
-		return self.id
-"""
+	def __str__(self):
+		return self.nombre
+
 class Almacen(models.Model):
 	anaqueles_por_fila = models.IntegerField()
 	direccion = models.TextField()
