1ab562d27f41f5191a92d7383c4762133d565691
Alex Baylon <aunesto15@gmail.com>
2017-05-15 18:57:04 -0500

    modelos actualizados 2.1

--- aplicacion/models.py
+++ aplicacion/models.py
@@ -37,6 +37,7 @@ class Producto(models.Model):
 	nombre = models.CharField(max_length = 100)
 	codigo = models.CharField(max_length = 10, null=True)
 	valor = models.DecimalField(max_digits = 10, decimal_places = 2)
+	ultima_clasificacion = models.CharField(max_length=2)
 
 	def __str__(self):
 		return self.nombre
