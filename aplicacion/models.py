7c32c32a6d022deaab0f1491f6bb25f7e0bb0757
Alex Baylon <aunesto15@gmail.com>
2017-05-19 20:55:14 -0500

    modelos actualizados 2.2

--- aplicacion/models.py
+++ aplicacion/models.py
@@ -32,12 +32,12 @@ class SubGrupo(models.Model):
 
 class Producto(models.Model):
 	id = models.AutoField(primary_key = True)
-	s_grupo = models.ForeignKey(SubGrupo, on_delete = models.SET_NULL, null=True)
+	s_grupo = models.ForeignKey(SubGrupo, on_delete = models.SET_NULL, null = True)
 
 	nombre = models.CharField(max_length = 100)
-	codigo = models.CharField(max_length = 10, null=True)
+	codigo = models.CharField(max_length = 10, null = True)
 	valor = models.DecimalField(max_digits = 10, decimal_places = 2)
-	ultima_clasificacion = models.CharField(max_length=2)
+	ultima_clasificacion = models.CharField(max_length=2, null = True)
 
 	def __str__(self):
 		return self.nombre
