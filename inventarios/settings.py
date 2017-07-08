da4e7c87c073651e08783c9b16339eac69756d00
eyvind1 <eyvind.osco@ucsp.edu.pe>
2017-07-07 20:27:54 -0500

    formato pep8

--- inventarios/settings.py
+++ inventarios/settings.py
@@ -80,8 +80,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'inventarios',
-        'USER': 'postgres',
-        'PASSWORD': '12ggwp12',
+        'USER': 'scrum',
+        'PASSWORD': '1234',
         'HOST': 'localhost',
         'PORT': '',
     }
